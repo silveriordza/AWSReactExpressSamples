6d595932a21649aae5b7dd6246e3b4ee1b24a003
silveriordza <silverio.rodriguez@kuarsis.com>
2021-11-28 17:27:28 -0500

    First connection of React APP with Redux to an AWS API Lambda NodeJs function

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,17 @@
+export type AmplifyDependentResourcesAttributes = {
+    "function": {
+        "awsReactExpressHelloFunction": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "api": {
+        "awsReactExpressHelloFunctionEndpointRestAPI": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
+        }
+    }
+}
